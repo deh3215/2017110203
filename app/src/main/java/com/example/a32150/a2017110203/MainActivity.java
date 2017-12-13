1c5a4f221a92330e9042036a931d400f9d72233e
deh3215 <3215009@gmail.com>
2017-12-13 14:43:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a32150/a2017110203/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a32150.a2017110203;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
